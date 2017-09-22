75828cbbb39999dbe452c8cc3300f6a42d3da6fb
Ciaran <ciaran426@yahoo.co.uk>
2017-09-22 12:03:21 +0100

    j

--- js/main.js
+++ js/main.js
@@ -24,15 +24,20 @@ $(document).ready(function(){
               <div class="well">
                 <div class="row">
                   <div class="col-md-7">
-                    <strong>${repo.name}</strong>
+                    <strong>${repo.name}</strong>: ${repo.description}
                   </div>
                   <div class="col-md-3">
+                    <span class="label label-default">Forks: ${repo.forks_count}</span>
+                    <span class="label label-primary">Watchers: ${repo.watchers_count}</span>
+                    <span class="label label-success">Stars: ${repo.stargazers_count}}</span>
+
                   </div>
                   <div class="col-md-2">
+                    <a href="${repo.html_url}" target="blank" class="btn btn-default">Repo page</a>
                   </div>
                 </div>
               </div>
-            `)
+            `);
           });
         });
 
@@ -48,17 +53,17 @@ $(document).ready(function(){
                 <a target="_blank" class="btn btn-primary btn-block" href="${user.html_url}">View Profile</a>
               </div>
               <div class="col-md-9">
-              <span class="label label-default">Public Repos: ${user.public_repos}</span>
-              <span class="label label-primary">Public Gists: ${user.public_gists}</span>
-              <span class="label label-success">Followers: ${user.followers}</span>
-              <span class="label label-info">Following: ${user.following}</span>
-              <br><br>
-              <ul class="list-group">
-                <li class="list-group-item">Company: ${user.company}</li>
-                <li class="list-group-item">Website/blog: ${user.blog}</li>
-                <li class="list-group-item">Location: ${user.location}</li>
-                <li class="list-group-item">Member Since: ${user.created_at}</li>
-              </ul>
+                <span class="label label-default">Public Repos: ${user.public_repos}</span>
+                <span class="label label-primary">Public Gists: ${user.public_gists}</span>
+                <span class="label label-success">Followers: ${user.followers}</span>
+                <span class="label label-info">Following: ${user.following}</span>
+                <br><br>
+                <ul class="list-group">
+                  <li class="list-group-item">Company: ${user.company}</li>
+                  <li class="list-group-item">Website/blog: ${user.blog}</li>
+                  <li class="list-group-item">Location: ${user.location}</li>
+                  <li class="list-group-item">Member Since: ${user.created_at}</li>
+                </ul>
               </div>
             </div>
           </div>
